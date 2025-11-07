2847f897c09159711d402412cad18d796880c271
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-11-07 12:31:38 +0530

    varibale

---
+++ Varibale.py
@@ -0,0 +1,11 @@
+# Rules
+# 1. Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# 2. The rest of the variable name can contain letters, digits (0-9), or underscores.
+# 3. Variable names are case-sensitive (e.g., myVar and myvar are different variables).
+# 4. Variable names cannot be the same as Python reserved keywords (e.g., if, else, while, etc.).
+my_variable = 10
+anotherVariable123 = "Hello, World!"
+_pi_value = 3.14
+print(my_variable)
+print(anotherVariable123)
+print(_pi_value)     
