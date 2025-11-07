55da203e7d57674b7950436fcb6d2b6a913e2c59
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-11-07 15:25:57 +0530

    A1 work done

---
+++ A1.0.py
@@ -0,0 +1,83 @@
+# Q1
+
+labour1="Mahesh"
+labour2="Mithilesh"
+labour3="Ramesh"
+labour4="Sumesh"
+
+print("Labour 1 name is : ",labour1)
+print("Labour 2 name is : ",labour2)
+print("Labour 3 name is : ",labour3)
+print("Labour 4 name is : ",labour4)
+
+#Q2
+
+labour1_wage=100
+labour2_wage=200
+labour3_wage=300
+labour4_wage=400
+
+print(f"labour1 name is : {labour1} ans labour1 wage is : {labour1_wage} . ")
+print(f"labour2 name is : {labour1} ans labour2 wage is : {labour2_wage} . ")
+print(f"labour3 name is : {labour1} ans labour3 wage is : {labour3_wage} . ")
+print(f"labour4 name is : {labour1} ans labour4 wage is : {labour4_wage} . ")
+
+
+#Q3
+
+print(    """ Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that
+    we are implemeting all the logics by ourself. The aim here is to build our "4 BHK" house with the 
+    help of 'Python programming'. We have total land is of \100 ft * 100ft /, to colmplete the house 
+    we have total 6 labours with 'different skill set like "\\ building wall or building roof \\".
+            I have to print this paragraph as it is given here.""")
+
+paragraph = "\"\"\" Programming aasan hai. We are going to learn this in depth. While learning we have to make sure that\n" \
+            "we are implemeting all the logics by ourself. The aim here is to build our \"4 BHK\" house with the \n" \
+            "help of 'Python programming'. We have total land is of \\100 ft * 100ft /, to colmplete the house \n" \
+            "we have total 6 labours with 'different skill set like \"\\\\ building wall or building roof \\\\\".\n" \
+            "\tI have to print this paragraph as it is given here.\"\"\""
+
+
+print(paragraph)
+
+
+#Q4
+
+#A NameError occurs in Python when you try to use a variable or function name that has not been defined.
+
+# print(Monish)
+
+#Q5
+# A high-level language means that Python is close to human language and far from machine language (binary or assembly).
+# It allows programmers to focus on logic, not on hardware details like memory management or processor instructions.
+
+#Q6
+# In a compiled language, the entire program is translated (compiled) into machine code before execution.
+# This machine code is what the computer’s CPU directly understands.
+# Fast execution (because code is already compiled).
+# Errors are found before running the program
+# Needs a compiler (like GCC for C).
+# Examples:
+# C
+# C++
+# Java (partly compiled, partly interpreted — uses JVM bytecode)
+# Go
+# Rust
+
+# In an interpreted language, the program is executed line by line by an interpreter — no need for a separate compilation step.
+# Easier to debug (errors appear as soon as the line runs).
+# Slower execution (interpreted each time).
+# More flexible and platform independent.
+# Examples:
+# Python
+# JavaScript
+# Ruby
+# PHP
+# Bash
+
+#Q7
+
+print(id(labour1),id(labour2),id(labour3),id(labour4),id(labour1_wage),id(labour2_wage),id(labour3_wage),id(labour4_wage))
+
+
+
