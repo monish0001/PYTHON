a019aa82dfaa001c2674ca81eb5a458a2b9fb711
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-11-13 20:03:26 +0530

    **kwargs convert all the things into dictonary

---
+++ parameter_keyword_argrument_kwargs.py
@@ -0,0 +1,31 @@
+# from loguru import logger
+
+# def generic_logging(**kwargs):
+#     for key, value in kwargs.items():
+#         print(key, value)
+
+
+# logger.info("first function call : ")
+
+# generic_logging(status="sucess", staus_code=200, message="successfully")
+
+# logger.info("second function call : ")
+
+# generic_logging(status="fail", staus_code=501, error="db connection fail")
+
+
+
+from loguru import logger
+
+def generic_logging(**kwargs):
+    
+    log_message = " | ".join([f"{key}={value}" for key, value in kwargs.items()])
+    logger.info(log_message)
+
+
+logger.info("first function call :")
+generic_logging(status="success", status_code=200, message="successfully")
+
+logger.info("second function call :")
+generic_logging(status="fail", status_code=501, error="db connection fail")
+
