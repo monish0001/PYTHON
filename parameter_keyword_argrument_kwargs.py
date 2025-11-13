d158817a6b7e1e085c47ae53802e0353d5aeebfc
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-11-13 20:15:01 +0530

    file write code added

--- parameter_keyword_argrument_kwargs.py
+++ parameter_keyword_argrument_kwargs.py
@@ -15,7 +15,29 @@
 
 
 
+# from loguru import logger
+
+# def generic_logging(**kwargs):
+    
+#     log_message = " | ".join([f"{key}={value}" for key, value in kwargs.items()])
+#     logger.info(log_message)
+
+
+# logger.info("first function call :")
+# generic_logging(status="success", status_code=200, message="successfully")
+
+# logger.info("second function call :")
+# generic_logging(status="fail", status_code=501, error="db connection fail")
+
+
+
+
 from loguru import logger
+
+
+logger.add("log_file.txt")
+
+
 
 def generic_logging(**kwargs):
     
@@ -26,6 +48,14 @@ def generic_logging(**kwargs):
 logger.info("first function call :")
 generic_logging(status="success", status_code=200, message="successfully")
 
-logger.info("second function call :")
+logger.error("second function call :")
 generic_logging(status="fail", status_code=501, error="db connection fail")
+
+
+
+
+
+
+
+
 
