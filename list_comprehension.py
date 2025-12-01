0a5a0cf8d9d72b1116b12a7746bcbc486637606a
Mohd Monish <32845817+monish0001@users.noreply.github.com>
2025-12-01 10:56:36 +0530

    list and dict

--- list_comprehension.py
+++ list_comprehension.py
@@ -50,7 +50,17 @@ logger.info(ans)
 
 
 
+# Python code to demonstrate dictionary 
+# comprehension
 
+# Lists to represent keys and values
+keys = ['a','b','c','d','e']
+values = [1,2,3,4,5]  
+
+# but this line shows dict comprehension here  
+myDict = { k:v for (k,v) in zip(keys, values)}  
+
+print (myDict)
 
 
 
